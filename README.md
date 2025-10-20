9d7b192c71b136f6cf15b2093df6085e378156da
Charles Knowles <CJUKnowles@gmail.com>
2025-10-20 15:13:32 +0100

    initalize project for github

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gamedev Workshop - Undertale
+A simple version of Undertale made in Godot for the PAL gamedev workshop.
+
+The main branch is intended as a model project for tutors and students to reference as needed. There are also branches for each term, containing weekly commits for students to use as catchup material.
--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,19 +1,19 @@
 extends CharacterBody2D
 
-# Export variables - values that can be easily changed from the inspector window
-@export var move_speed:float = 100
+# Export variables - can be easily changed from the inspector window
+@export var move_speed:float = 300
 
-# Global variables - accessible by all functions
-var input:Vector2 # Defines what WASD keys the player is current pressing. (0,0) is no movement, (1,0) is rightward, and so on.
+# Global variables - accessible by all functions in this script
+var movement_input:Vector2 # Defines what direction the player is trying to move
 
 # Called every frame (framerate varies)
-func _process(delta):
-	input = Vector2(
+func _process(_delta):
+	movement_input = Vector2(						# Type is Vector2 (x, y)
 		Input.get_axis("move_left", "move_right"),	# Returns 1 to move right, -1 to move left
 		Input.get_axis("move_up", "move_down")		# Returns 1 to move up, -1 to move down
 		).normalized() 								# Normalize the input vector (diagonal movement should have magnitude of 1)
 	
 # Called every physics update (consistent rate)
-func _physics_process(delta):
-	velocity = input * move_speed 	# Set player's velocity (does not move them yet)
-	move_and_slide() 				# Tell player to move based on their current velocity
+func _physics_process(_delta):
+	velocity = movement_input * move_speed 		# Set player velocity based on most recent input
+	move_and_slide() 							# Move player based on current velocity
