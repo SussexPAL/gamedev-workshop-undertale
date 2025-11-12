11444dbb2ba991c6f198d01476f99797387e02ba
Charles Knowles <CJUKnowles@gmail.com>
2025-11-12 13:59:38 +0000

    basic button, basic battle scene

--- scenes/interactable_area.gd
+++ scenes/interactable_area.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var player_nearby:bool = false # true if player is within interaction range
+@export var battle_scene:PackedScene # A reference to the battle scene
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):	# a;sdlkfja;sldkfj;asldkj
@@ -13,4 +14,5 @@ func _on_body_exited(body):
 
 func _on_player_player_interact_request():
 	if player_nearby:
+		get_tree().change_scene_to_packed(battle_scene)
 		print("Successful interact!")
---
+++ scripts/cool_button.gd
@@ -0,0 +1,6 @@
+extends Button
+
+
+func _on_pressed():
+	print("The Cool Button has been Pressed")
+	pass # Replace with function body.
